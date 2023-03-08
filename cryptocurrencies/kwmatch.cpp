f9115d98a7001adfe2ba9b4d895117bbc4aebd73
Gerard Wagener <gerard.wagener@circl.lu>
2023-03-08 08:44:56 +0100

    add:[NISDUC] added command line options for kwmatch

--- cryptocurrencies/kwmatch.cpp
+++ cryptocurrencies/kwmatch.cpp
@@ -27,6 +27,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <getopt.h>
 
 using namespace std;
 
@@ -79,7 +80,58 @@ bool load_regexp_file(char const* filename)
     return false;
 }
 
+void usage(void)
+{
+    cout << "kwmatch - matches a set of regex defined in a file <file> on stdin"<<endl;
+    cout << "kwmatch [-h] [-f filename] [-n name] [-o original] "<< endl;
+    cout << endl<< "OPTIONS" << endl<<endl;
+    cout <<"-h    --help     Shows this screen"<<endl;
+    cout <<"-f    --file     File containing the regular expressions" <<endl;
+    cout <<"-n    --name     Name where stdin came from i.e. compressed filename of the shard or URL" <<endl;
+    cout <<"-o    --original Original name of the data" <<endl;
+
+    cout << endl << "FORMAT of the regexp file" << endl << endl;
+    cout << "One line contains a regexp with spaces seprated" << endl;
+    cout << "Spaces are not forseen in names of regexp but they can be in the regexp itself" << endl;
+    cout << "The following fields are expected per line" << endl;
+    cout << "Format id flag name regexp" <<endl;
+}
+
 int main(int argc, char* argv[])
 {
+    int next_option = 0;
+    const char* const short_options = "hf:n:t:o";
+    const struct option long_options[] = {
+                { "help", 0, NULL, 'h' },
+                { "file", 1, NULL, 'f' },
+                { "name", 1, NULL, 'n' },
+                { "orginal", 1, NULL, 'o'},
+                {NULL,0,NULL,0}
+   };
+
+ do {
+        next_option = getopt_long (argc, argv, short_options,
+                                   long_options, NULL);
+        if (next_option > 0) {
+            switch(next_option)
+            {
+            case 'h':
+                usage();
+                return EXIT_SUCCESS;
+            case 'f':
+                cout << "file:"<<optarg<<endl;
+                break;
+            case 'n':
+                break;
+            case 'o':
+                break;
+            default:
+                /* Something unexpected happended */
+                return EXIT_FAILURE;
+            }
+        }
+ } while(next_option != -1);
+
+
 	return EXIT_SUCCESS;
 }
