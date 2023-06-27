d6e6d9edcd94462434e53742101b260ba104d590
Gerard Wagener <gerard.wagener@circl.lu>
2023-06-27 11:38:35 +0200

    chg:[onions] check if file exists

--- onions/process.sh
+++ onions/process.sh
@@ -4,5 +4,12 @@ FILENAME=$1
 #Search onions with a rough pass onions then tansform or control characters to newlines
 # and do a second path
 F="`basename $FILENAME | sed 's/.gz/.onions/g'`"
+TF="./results/$F"
+
+
+if [ -e $TF ]; then
+        echo "[INFO] File $FILENAME was already processed"
+        exit 0
+fi
 
 zcat $FILENAME | grep -a onion | tr ' "><' '\n' | grep "\.onion$" > ./results/$F
