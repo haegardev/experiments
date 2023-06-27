c03b7e12cf85298ab78ebc6f683cd5d404023946
Gerard Wagener <gerard.wagener@circl.lu>
2023-06-27 16:29:24 +0200

    chg:[onions] need to check max files in the loop

--- onions/download.sh
+++ onions/download.sh
@@ -2,7 +2,7 @@
 #Download sequentialy common crawl data in parallel process it and remove the
 #files. To not fill up the disks  the number of warcs can be fixed
 CRAWL_NUM="CC-MAIN-2023-23"
-MAX_FILES=100
+MAX_FILES=3
 PROCESSED_URLS="processed_urls.txt"
 PIDFILE="download.pid"
 
@@ -27,15 +27,6 @@ if [ ! -e warc.paths.gz ]; then
     wget https://data.commoncrawl.org/crawl-data/$CRAWL_NUM/warc.paths.gz
 fi
 
-#Check maximum number of files in download folder
-
-N=$(ls tmp/ -1 | wc -l | tr -d '\n')
-
-if [ $N -gt $MAX_FILES ]; then
-    echo "[INFO] found $N files. Max files = $MAX_FILES going to exit"
-    exit 0
-fi
-
 for i in `zcat warc.paths.gz`; do
     if [ ! -z "$(grep $i $PROCESSED_URLS)" ]; then
         echo "[INFO] file $i was already processed"
@@ -46,7 +37,17 @@ for i in `zcat warc.paths.gz`; do
         echo "[INFO] file $i was already downloaded"
         continue
     fi
+
+    #Check maximum number of files in download folder
+    N=$(ls tmp/ -1 | wc -l | tr -d '\n')
+
+    if [ $N -gt $MAX_FILES ]; then
+        echo "[INFO] found $N files. Max files = $MAX_FILES going to exit"
+        break
+    fi
+
     URL="https://data.commoncrawl.org/$i"
+
     nice -n 19 ionice -c 3 wget $URL -O "./tmp/.$f"
     mv "./tmp/.$f" "tmp/$f"
 done
