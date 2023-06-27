3bb0b1e004416d7a2437d5e12ef5c17278f35e1e
Gerard Wagener <gerard.wagener@circl.lu>
2023-06-27 11:46:18 +0200

    chg:[onions] download files in hidden files to avoid processing on partial files

--- onions/download.sh
+++ onions/download.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#Download sequentialy common crawl data and extratc onions
-
+#Download sequentialy common crawl data in parallel process it and remove the
+#files. To not fill up the disks  the number of warcs can be fixed
+CRAWL_NUM="CC-MAIN-2023-06"
+MAX_FILES=100
 
 if [ ! -e "./tmp" ]; then
     mdkir ./tmp
@@ -12,13 +14,14 @@ if [ ! -e "./results" ]; then
 fi
 
 if [ ! -e warc.paths.gz ]; then
-    wget https://data.commoncrawl.org/crawl-data/CC-MAIN-2023-06/warc.paths.gz
+    wget https://data.commoncrawl.org/crawl-data/$CRAWL_NUM/warc.paths.gz
 fi
 
 for i in `zcat warc.paths.gz`; do
     f=`basename $i`
     URL="https://data.commoncrawl.org/$i"
-    wget $URL -O "tmp/$f"
+    wget $URL -O "./tmp/.$f"
+    mv "./tmp/.$f" "tmp/$f"
 done
 
 
