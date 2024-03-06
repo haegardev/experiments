2be5496251d48c8f0bc4f3ca2f71bc8e3fcba98a
Gerard Wagener <gerard.wagener@circl.lu>
2024-03-06 10:21:24 +0100

    chg:[pcap] need the header to serialize

---
+++ pcaps/count/PcapCountHeader.h
@@ -0,0 +1,41 @@
+#ifndef PCAPCOUNTHEADER_H
+#define PCAPCOUNTHEADER_H
+#include <stdint.h>
+#include <string>
+#include <map>
+using namespace std;
+#define VERSION "1.0"
+#define DESCRIPTION "PcapCount File"
+
+class PcapCountHeader {
+public:
+    // Serialization function template declaration
+    template<class Archive>
+     void serialize(Archive &ar, const unsigned int ) {
+        ar & description;
+        ar & pcapCountVersion;
+        ar & source;
+        ar & target;
+        ar & target_srcip_file;
+        ar & flag_src_ips;
+        ar & counted_data;
+    }
+
+    // Attributes
+    string description = DESCRIPTION;
+    string pcapCountVersion = VERSION;
+    map <uint32_t, uint32_t> counted_data;
+    //Count IP protocols observed
+    map <uint8_t,uint32_t> cnt_proto;
+    // Count IP destinations observed
+    map <uint32_t, uint32_t> cnt_ip_dst;
+    string source;
+    string target;
+    string target_srcip_file;
+    bool flag_src_ips;
+    uint32_t firstSeen;
+    uint32_t lastSeen;
+};
+
+#endif /* PCAPCOUNTHEADER_H */
+
