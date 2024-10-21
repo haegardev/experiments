7236db2e085f9d9c90886ccc3db205b5fe7de6c8
Gerard Wagener <gerard.wagener@circl.lu>
2024-10-21 14:44:14 +0200

    add:[lunog] added script to create filelist per days
    Those list can then be used in parallel with basic-stats

---
+++ lunog/overview/create-file-lists-per-day.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+SOURCE=$1
+TARGET=$2
+
+if [ -z "$SOURCE" ]; then
+    echo "A source with the tshark output must be there"
+    exit 1
+fi
+
+if [ -z "$TARGET" ]; then
+    echo "A directory where the filelist should be generated must be specified"
+    exit 1
+fi
+
+
+FILELIST=`mktemp`
+OUT="output"
+mkdir -p $OUT
+
+find $SOURCE -type f | sort >$FILELIST
+
+START=`head -n 1 $FILELIST | cut -d '-' -f 2,3,4`
+END=`tail -n 1 $FILELIST | cut -d '-' -f 2,3,4`
+
+T0=`date -d $START +%s`
+T1=`date -d  $END +%s`
+
+I=$T0
+SUF=".lst"
+while [ $I -le $T1 ]; do
+    DAY=`date -d @$I +%Y-%m-%d`
+    cat $FILELIST  |  grep "$DAY" > "$OUT/$DAY$SUF"
+    let I+=86400
+done
+
