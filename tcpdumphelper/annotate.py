56bf8fd44ba57e9d566762b9d0ea542109516067
Alexandre Dulaunoy <a@foo.be>
2024-11-15 10:33:19 +0100

    chg: [tcpdump annotation] Updated to support GeoOpen source with ASN number + add an option to exclude RFC1918 addresses

--- tcpdumphelper/annotate.py
+++ tcpdumphelper/annotate.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
+#
+# Usage: tcpdump -n -tttt -r brol.cap | python3 annotate.py --mmdb=2024-11-12-GeoOpen-Country-ASN.mmdb
 
 import re
 import sys
-import geoip2.database
+import maxminddb
 import argparse
 import pprint
+import ipaddress
 
-parser = argparse.ArgumentParser(description="Read tcpdump output with the -tttt option from stdin and annotate IP addresses")
+parser = argparse.ArgumentParser(
+    description="Read tcpdump output with the -tttt option from stdin and annotate IP addresses"
+)
 parser.add_argument("--mmdb", type=str, help="The MMDB file")
+parser.add_argument(
+    "--dont-skip-rfc1918",
+    action="store_true",
+    default=False,
+    help="Don't skip private networks defined in RFC1918",
+)
 args = parser.parse_args()
+
 
 def annotate_logs_with_country(mmdb_file_path):
     """
@@ -18,31 +30,31 @@ def annotate_logs_with_country(mmdb_file_path):
     # Compile a regex to match the first IP address in each line
     ip_regex = re.compile(r'\b\d{1,3}(?:\.\d{1,3}){3}\b')
     # Open the MaxMind database and process the input
-    with geoip2.database.Reader(args.mmdb) as reader:
+    with maxminddb.open_database(args.mmdb) as reader:
         for line in sys.stdin:
             line = line.strip()  # Remove trailing whitespace
             match = ip_regex.search(line)
             if match:
                 ip_address = match.group()
-                try:
-                    # Get the country code for the IP address
-                    response = reader.country(ip_address)
-                    #asn=response.AutonomousSystemNumber
-                    country_code = response.country.iso_code
-                    #asn=response.country.autonomousSystemNumber
-                    # Append the country code to the IP address
-                    asn=1
-                    annotated_line = line.replace(ip_address, f"{ip_address} [{country_code},{asn}]")
-                except geoip2.errors.AddressNotFoundError:
-                    annotated_line = line.replace(ip_address, f"{ip_address} [UNKNOWN]")
-                #except Exception as e:
-                #    annotated_line = line + f" [ERROR: {str(e)}]"
+                if (
+                    not args.dont_skip_rfc1918
+                    and ipaddress.ip_address(ip_address).is_private
+                ):
+                    continue
+                response = reader.get(ip_address)
+                country_code = response['country']['iso_code']
+                asn = response['country']['AutonomousSystemNumber']
+                asn_name = response['country']['AutonomousSystemOrganization']
+                annotated_line = line.replace(
+                    ip_address, f"{ip_address} [{country_code},{asn},{asn_name}]"
+                )
             else:
-                annotated_line = line  # If no IP address is found, keep the line unchanged
-            # Print the annotated line to standard output
-            #print(annotated_line)
+                annotated_line = (
+                    line  # If no IP address is found, keep the line unchanged
+                )
+            print(annotated_line)
+
 
 # Run the script
 if __name__ == "__main__":
     annotate_logs_with_country(args.mmdb)
-
